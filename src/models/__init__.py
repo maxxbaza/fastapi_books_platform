50fc3683883877622d421f76f22f2fd94557e692
Bazoev Maxim Vladimirovich <mvbazoev@edu.hse.ru>
2026-03-12 14:21:13 +0300

    make main empty

--- src/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .books import Book
-from .sellers import Seller
-
-__all__ = ["Book", "Seller"]
--- src/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import books, sellers
-from .books import *
-from .sellers import *
-
-__all__ = [*books.__all__, *sellers.__all__]
